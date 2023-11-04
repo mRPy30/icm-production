a034b9ed66bb39231bf853db133724125d4b352a
adobongtao <135185279+adobongtao@users.noreply.github.com>
2023-11-04 23:04:38 +0800

    Update calendar.php

--- client/calendar.php
+++ client/calendar.php
@@ -38,6 +38,10 @@ $page = $components[2];
 </head>
     
 <body>
+<div class="background">
+        <img src="../picture/logo.png">
+        <i class="fa-regular fa-bell"></i>
+    </div>  
     <?php 
         include '../client/sidebar.php';
     ?>
