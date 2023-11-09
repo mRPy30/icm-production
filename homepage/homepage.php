d90dc089526744c143613bd4cbcfdc47b12c4dfa
Maezelle Secuya <maezelle.secuya@cvsu.edu.ph>
2023-11-09 14:34:28 +0800

    heeeee

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -42,14 +42,19 @@
 
     <!----Main Content----->
 
-    <main class="homepage_main">
-        <div class="main-content">
-            <div class="image-content">
-                <img src="../picture/ICM.png" href="#home">
-            </div>
+    <section>
+        <div class="main-content" stylesheet="">
+            <svg id="curve" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01">
+                <path class="cls-1" d="M0,280.8S283.66,59,608.94,163.56s437.93,150.57,808,10.34V309.54H0V280.8Z"
+                    transform="translate(0-135.53)" />
+            </svg>
         </div>
+    </section>
+    <section class="sec2">
 
-    </main>
+    </section>
+
+
     <!----End of Main Content----->
 
 </body>
